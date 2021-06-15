b5297544fb372fffdedbd244436826fd3970bb83
gurrium <gurrium@hatena.ne.jp>
2021-06-15 20:16:38 +0900

    n日目 次は Error Handling

--- swift_tour.playground/Contents.swift
+++ swift_tour.playground/Contents.swift
@@ -1,12 +1,10 @@
-let beef = 0xbeef.ep2
-let cofee = 0xc0fee.ep12
-print(beef)
-print(cofee)
+let tuple = (1, 2)
+print(tuple.0)
 
-let dividedDec = 1_0_0_00_0.3_______1
-print(dividedDec)
-
-let tooBig: UInt8 = 255 + UInt8.random(in: 1...5)
-print(tooBig)
-
-let convertetd = UInt16(tooBig)
+var iuoString: String! = "implicitly unwrapped optional string"
+var oString = iuoString
+oString = nil
+iuoString = nil
+print(oString) // 落ちない
+print(iuoString) // 落ちない
+print(iuoString.count) // 落ちる
