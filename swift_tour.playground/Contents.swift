152ebfe0613526f4b87b7dc48c35a1d991a56208
gurrium <gurrium@hatena.ne.jp>
2021-06-17 20:33:58 +0900

    n日目 次はBasic Operators

--- swift_tour.playground/Contents.swift
+++ swift_tour.playground/Contents.swift
@@ -1,13 +1,3 @@
-let tuple = (1, 2)
-print(tuple.0)
+let age = 0
 
-var iuoString: String! = "implicitly unwrapped optional string"
-var oString = iuoString
-iuoString = nil
-oString = nil
-print(iuoString) // 落ちない
-print(oString) // 落ちない
-print(iuoString?.count) // 落ちない
-print(oString?.count) // 落ちない
-print(iuoString.count) // 落ちる
-//print(oString.count) // コンパイルできない
+assert(age != 0)
