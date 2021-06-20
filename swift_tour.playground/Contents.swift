ef4890c3ee952399631d451a085057d30360728a
gurrium <gurrium@hatena.ne.jp>
2021-06-20 18:39:42 +0900

    n日目 次はLogical Operators

--- swift_tour.playground/Contents.swift
+++ swift_tour.playground/Contents.swift
@@ -1,18 +1,8 @@
-let a = false
-//print(a!)
-print(-5 % 3)
+let hoge = Bool.random() ? "hoge" : "HOGE"
 
-(1, 2) < (2, 1)
+print(hoge)
 
-func lt<T: Comparable, U: Comparable>(_ lhs: (T, U), _ rhs: (T, U)) -> Bool {
-    if lhs.0 != rhs.0 {
-        return lhs.0 < rhs.0
-    } else {
-        return lhs.1 < rhs.1
-    }
+let nums = [0, 1, 2, 3]
+for num in nums[...1] {
+    print(num)
 }
-
-assert(((1, 1) < (2, 2)) == lt((1, 1), (2, 2)))
-assert(((1, 2) < (2, 1)) == lt((1, 2), (2, 1)))
-assert(((2, 1) < (1, 2)) == lt((2, 1), (1, 2)))
-assert(((2, 2) < (1, 1)) == lt((2, 2), (1, 1)))
