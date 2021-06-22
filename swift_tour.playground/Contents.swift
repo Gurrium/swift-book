892d359f1504d0c79549f949ca7ae7a1d6505514
gurrium <gurrium@hatena.ne.jp>
2021-06-22 20:29:56 +0900

    n日目 次はInitializing an Empty String

--- swift_tour.playground/Contents.swift
+++ swift_tour.playground/Contents.swift
@@ -1,19 +1,21 @@
-var conditions: [[Bool]] = []
+let singleLineString = "These are the same."
+let multilineString = """
+These are the same.
+"""
 
-for i in 0b000...0b111 {
-    conditions.append([
-        (i >> 2) & 0b001 == 1,
-        (i >> 1) & 0b001 == 1,
-        (i >> 0) & 0b001 == 1,
-    ])
-}
+print(singleLineString)
+print(multilineString)
 
-for condition in conditions {
-    print(condition)
-    print(echo(condition[0]) || echo(condition[1]) && echo(condition[2]))
-}
+let threeDoubleQuotationMarks = """
+Escaping the first quotation mark \"""
+Escaping the second quotation mark "\""
+Escaping the third quotation mark ""\"
+Escaping all three quotation marks \"\"\"
+"""
 
-func echo(_ some: Bool) -> Bool {
-    print("called echo with: \(some)")
-    return some
-}
+print(threeDoubleQuotationMarks)
+print(##"Line 1\##nLine 2"##)
+print(#"""
+hoge\\#n"""
+fuga
+"""#)
