c7ffae49e86e8341145418dde850e3b7168d068a
gurrium <gurrium@hatena.ne.jp>
2021-06-25 21:31:53 +0900

    例を足したがわからん

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -6,7 +6,11 @@ var str: String? = "Hello, World!!"
 let substr = str![..<str!.firstIndex(of: ",")!]
 
 printPointer(of: str)
+print(MemoryLayout.size(ofValue: str))
 printPointer(of: substr)
+print(MemoryLayout.size(ofValue: substr))
 str = nil
 printPointer(of: str)
+print(MemoryLayout.size(ofValue: str))
 printPointer(of: substr)
+print(MemoryLayout.size(ofValue: substr))
