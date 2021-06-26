0b8753c4791b9468ae52a0c2ca3cdc0dddb4e040
gurrium <gurrium@hatena.ne.jp>
2021-06-27 00:06:13 +0900

    例を更新

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,19 +1,5 @@
-// "Voulez-vous un café?" using LATIN SMALL LETTER E WITH ACUTE
-let eAcuteQuestion = "Voulez-vous un caf\u{E9}?"
+let café = "caf\u{e9}"
+let cafe = "cafe\u{301}"
 
-// "Voulez-vous un café?" using LATIN SMALL LETTER E and COMBINING ACUTE ACCENT
-let combinedEAcuteQuestion = "Voulez-vous un caf\u{65}\u{301}?"
-
-if eAcuteQuestion == combinedEAcuteQuestion {
-    print("These two strings are considered equal")
-}
-// Prints "These two strings are considered equal"
-
-let latinCapitalLetterA: Character = "\u{41}"
-
-let cyrillicCapitalLetterA: Character = "\u{0410}"
-
-if latinCapitalLetterA != cyrillicCapitalLetterA {
-    print("These two characters aren't equivalent.")
-}
-// Prints "These two characters aren't equivalent."
+print(cafe == café)
+print("\u{41}" == "\u{410}")
