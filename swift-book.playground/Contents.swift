3fb990806c577cd5a772af31281dfc67a75f9c00
gurrium <gurrium@hatena.ne.jp>
2021-06-28 22:18:44 +0900

    n日目 次はFunctions With an Implicit Return

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,6 +1,17 @@
-for codeUnit in "Dog‼🐶".utf16 {
-    print(codeUnit)
-//    print(String(UnicodeScalar(codeUnit)!))
+func minMax(array: [Int]) -> (min: Int, max: Int)? {
+    if let min = array.min(),
+       let max = array.max() {
+//        return (min, max)
+        return (min: min, max: max)
+    } else {
+        return nil
+    }
 }
 
-"hoge".unicodeScalars
+minMax(array: Array(0...10))?.min
+
+let minMax: (min: Int, max: Int)
+minMax = (1, 2)
+minMax.min
+
+// let minMachs: (min: Int, machs: Int) = minMax
