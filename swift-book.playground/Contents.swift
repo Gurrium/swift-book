b2d22ba3f407b2ce5269410a83602bcfe3a88feb
gurrium <gurrium@hatena.ne.jp>
2021-07-02 20:57:38 +0900

    n日目 次はAutoClosures

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,8 +1,37 @@
-let names = ["fuga", "hoge", "piyo"]
-let checker = { (_ seman: [String]) in assert(names.reversed().elementsEqual(seman)) }
+var completionHandlers: [() -> Void] = []
+func someFunctionWithEscapingClosure(completionHandler: @escaping () -> Void) {
+    completionHandlers.append(completionHandler)
+}
+func someFunctionWithNonescapingClosure(closure: () -> Void) {
+    closure()
+}
 
-checker(names.sorted(by: { (s0: String, s1: String) -> Bool in return s0 > s1 }))
-checker(names.sorted(by: { s0, s1 in return s0 > s1 }))
-checker(names.sorted(by: { s0, s1 in s0 > s1 }))
-checker(names.sorted(by: { $0 > $1 }))
-checker(names.sorted(by: >))
+print("SomeClass")
+class SomeClass {
+    var x = 10
+    func doSomething() {
+        print(x) // => 10
+        someFunctionWithEscapingClosure { self.x = 100 } // (1)
+        someFunctionWithEscapingClosure { [self] in x = 200 } // (2)
+        completionHandlers.forEach { closure in
+            closure()
+            print(x)
+        }
+        // => 100
+        // => 200
+    }
+}
+SomeClass().doSomething()
+
+print("SomeStruct")
+struct SomeStruct {
+    var x = 10
+    mutating func doSomething() {
+        print(x) // => 10
+        someFunctionWithNonescapingClosure { x = 200 }  // (3)
+        print(x) // => 200
+        // someFunctionWithEscapingClosure { x = 100 }  // (4)
+    }
+}
+var some = SomeStruct()
+some.doSomething()
