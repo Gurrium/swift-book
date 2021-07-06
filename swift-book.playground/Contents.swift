87684c84036c98558566a0b465973b03e9af83be
gurrium <gurrium@hatena.ne.jp>
2021-07-06 23:30:32 +0900

    n日目 https://giarrium.hatenablog.com/entry/2021/07/06/233020?_ga=2.60494761.545749773.1625580929-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,59 +1,27 @@
-enum ArithmeticExpression {
-    case number(Int)
-    indirect case addition(ArithmeticExpression, ArithmeticExpression)
-    indirect case multiplication(ArithmeticExpression, ArithmeticExpression)
-}
-
-func evaluate(_ expression: ArithmeticExpression) -> Int {
-    switch expression {
-    case let .number(value):
-        return value
-    case let .addition(left, right):
-        return evaluate(left) + evaluate(right)
-    case let .multiplication(left, right):
-        return evaluate(left) * evaluate(right)
+func check(_ actual: [String]) {
+    var expected: [String] = []
+    for i in 1...100 {
+        let tmp: String = {
+            if i % 3 == 0 {
+                return i % 5 == 0 ? "fizzbuzz" : "fizz"
+            }else  if i % 5 == 0 {
+                return "buzz"
+            } else {
+                return "\(i)"
+            }
+        }()
+        expected.append(tmp)
     }
-}
 
-let five = ArithmeticExpression.number(5)
-let four = ArithmeticExpression.number(4)
-let sum = ArithmeticExpression.addition(five, four)
-let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
-print(evaluate(product))
-
-indirect enum BinTree {
-    case inner(left: BinTree, right: BinTree, value: Int)
-    case leaf(value: Int)
-}
-
-func inOrderTraverse(_ tree: BinTree) {
-    switch tree {
-    case let .inner(left, right, value):
-        inOrderTraverse(left)
-        print(value)
-        inOrderTraverse(right)
-    case let .leaf(value):
-        print(value)
+    for (e, a) in zip(expected, actual) {
+        print(e, a, separator: " ")
     }
+
+    assert(expected.elementsEqual(actual))
 }
 
-let tree = BinTree.inner(
-    left: .inner(
-        left: .leaf(value: 0),
-        right: .inner(
-            left: .leaf(value: 2),
-            right: .leaf(value: 4),
-            value: 3),
-        value: 1),
-    right: .leaf(value: 6),
-    value: 5)
+//let actual = (1...100).map {"\($0 % 3 == 0 ? "fizz" : ($0 % 5 != 0 ? "\($0)" : ""))\($0 % 5 == 0 ? "buzz" : "")" }
+let actual = (1...100).map{($0 % 3==0 ?"fizz":($0 % 5==0 ?"":"\($0)"))+($0 % 5==0 ?"buzz":"")}
+//let actual = (1...100).map {"\($0 % 3 == 0 ? "fizz" : ($0 % 5 != 0 ? "\($0)" : ""))\($0 % 5 == 0 ? "buzz" : "")" }
 
-inOrderTraverse(tree)
-// =>
-// 0
-// 1
-// 2
-// 3
-// 4
-// 5
-// 6
+check(actual)
