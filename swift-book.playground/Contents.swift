5bbc21ca63270b14c71aa419b82f379601298c09
gurrium <gurrium@hatena.ne.jp>
2021-07-13 20:45:39 +0900

    day 36 https://giarrium.hatenablog.com/entry/2021/07/13/204522?_ga=2.44552429.837816334.1626010675-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,43 +1,24 @@
-@propertyWrapper
-struct SmallNumber {
-    private var number = 0
-    var projectedValue = false
-    var wrappedValue: Int {
-        get { return number }
-        set {
-            if newValue > 12 {
-                number = 12
-                projectedValue = true
-            } else {
-                number = newValue
-                projectedValue = false
-            }
-        }
+class SomeClass {
+    static var storedTypeProperty = "Some value."
+//    class var overrideableStoredTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 27
+    }
+    class var overrideableComputedTypeProperty: Int {
+        return 107
     }
 }
 
-enum Size {
-    case small, large
-}
-
-struct SizedRectangle {
-    @SmallNumber var height: Int
-    @SmallNumber var width: Int
-
-    mutating func resize(to size: Size) -> Bool {
-        switch size {
-        case .small:
-            height = 10
-            width = 20
-        case .large:
-            height = 100
-            width = 100
-        }
-        return $height || $width
+class SubClass: SomeClass {
+    override static var overrideableComputedTypeProperty: Int {
+        2
     }
 }
 
-var sized = SizedRectangle()
-print(sized.resize(to: .large), sized.$width, sized.$height)
-print(sized.resize(to: .small), sized.$width, sized.$height)
-
+class SubSubClass: SubClass {
+//    SubClassでstatic varとなっているのでこれはできない
+//    override static var overrideableComputedTypeProperty: Int {
+//        2
+//    }
+}
+print(SubClass.overrideableComputedTypeProperty) // => 2
