0594a5e459feb212da862675f37a68cb93f39b28
gurrium <gurrium@hatena.ne.jp>
2021-07-15 20:28:22 +0900

    day 38 https://giarrium.hatenablog.com/entry/2021/07/15/202813?_ga=2.48829551.837816334.1626010675-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,10 +1,49 @@
-struct Point {
-    var x = 0.0, y = 0.0
-    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
-        x += deltaX
-        y += deltaY
+struct LevelTracker {
+    static var highestUnlockedLevel = 1
+    var currentLevel = 1
+
+    static func unlock(_ level: Int) {
+        if level > highestUnlockedLevel { highestUnlockedLevel = level }
+    }
+
+    static func isUnlocked(_ level: Int) -> Bool {
+        return level <= highestUnlockedLevel
+    }
+
+    @discardableResult
+    mutating func advance(to level: Int) -> Bool {
+        if LevelTracker.isUnlocked(level) {
+            currentLevel = level
+            return true
+        } else {
+            return false
+        }
     }
 }
-var somePoint = Point(x: 1.0, y: 1.0)
-somePoint.moveBy(x: 2.0, y: 3.0)
-print("The point is now at (\(somePoint.x), \(somePoint.y))")
+
+class Player {
+    var tracker = LevelTracker()
+    let playerName: String
+
+    func complete(level: Int) {
+        LevelTracker.unlock(level + 1)
+        tracker.advance(to: level + 1)
+    }
+
+    init(name: String) {
+        playerName = name
+    }
+}
+
+var player = Player(name: "Argyrios")
+player.complete(level: 1)
+print("highest unlocked level is now \(LevelTracker.highestUnlockedLevel)")
+// Prints "highest unlocked level is now 2"
+
+player = Player(name: "Beto")
+if player.tracker.advance(to: 6) {
+    print("player is now on level 6")
+} else {
+    print("level 6 hasn't yet been unlocked")
+}
+// Prints "level 6 hasn't yet been unlocked"
