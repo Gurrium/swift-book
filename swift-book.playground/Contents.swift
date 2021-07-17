3ff3d67ea619ae877b1586865ab10129f6fc700f
gurrium <gurrium@hatena.ne.jp>
2021-07-17 22:36:10 +0900

    day 40 https://giarrium.hatenablog.com/entry/2021/07/17/223555?_ga=2.89146588.634020653.1626528722-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,15 +1,39 @@
-struct TimesTable {
-    let multiplier: Int
-    subscript(index: Int) -> Int {
-        return multiplier * index
-    }
-}
-let threeTimesTable = TimesTable(multiplier: 3)
-print("six times three is \(threeTimesTable[6])")
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
 
-struct Some {
-    subscript(table: TimesTable) -> Int {
-        return table.multiplier
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(repeating: 0.0, count: rows * columns)
+    }
+
+    func indexIsValid(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+
+    func inspect() {
+        for i in 0..<rows {
+            for j in 0..<columns {
+                print(grid[i * columns + j], terminator: " ")
+            }
+            print()
+        }
+    }
+
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
     }
 }
-Some()[threeTimesTable]
+
+var twoByTwo = Matrix(rows: 2, columns: 2)
+twoByTwo.inspect()
+twoByTwo[0, 1] = 2.5
+twoByTwo.inspect()
