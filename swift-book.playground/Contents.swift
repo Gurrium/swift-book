257c4c8d937515d1cefb274a9fcc33fa3fbdfd0a
gurrium <gurrium@hatena.ne.jp>
2021-07-18 23:50:47 +0900

    day 41 https://giarrium.hatenablog.com/entry/2021/07/18/235041?_ga=2.53133992.1449300841.1626619295-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,39 +1,23 @@
-struct Matrix {
-    let rows: Int, columns: Int
-    var grid: [Double]
-
-    init(rows: Int, columns: Int) {
-        self.rows = rows
-        self.columns = columns
-        grid = Array(repeating: 0.0, count: rows * columns)
+enum Planet: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+    static subscript(n: Int) -> Planet {
+        return Planet(rawValue: n)!
     }
+}
+let mars = Planet[4]
+print(mars)
 
-    func indexIsValid(row: Int, column: Int) -> Bool {
-        return row >= 0 && row < rows && column >= 0 && column < columns
-    }
+enum Env {
+    static var envs: [String: String] = [:]
 
-    func inspect() {
-        for i in 0..<rows {
-            for j in 0..<columns {
-                print(grid[i * columns + j], terminator: " ")
-            }
-            print()
-        }
-    }
-
-    subscript(row: Int, column: Int) -> Double {
+    static subscript(_ name: String) -> String? {
         get {
-            assert(indexIsValid(row: row, column: column), "Index out of range")
-            return grid[(row * columns) + column]
+            return envs[name]
         }
         set {
-            assert(indexIsValid(row: row, column: column), "Index out of range")
-            grid[(row * columns) + column] = newValue
+            envs[name] = newValue
         }
     }
 }
-
-var twoByTwo = Matrix(rows: 2, columns: 2)
-twoByTwo.inspect()
-twoByTwo[0, 1] = 2.5
-twoByTwo.inspect()
+Env["hoge"] = "hoge"
+print(Env["hoge"])
