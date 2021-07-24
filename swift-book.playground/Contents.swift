30b8e1c7ea1dd9eaa9d9465c42360e067a40f302
gurrium <gurrium@hatena.ne.jp>
2021-07-24 20:46:40 +0900

    day 47 https://giarrium.hatenablog.com/entry/2021/07/24/204628?_ga=2.197405222.866382795.1626957627-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,10 +1,35 @@
-
-import Foundation
-class ShoppingListItem {
-    let id = UUID()
-    var name: String?
-    var quantity = 1
-    var purchased = false
+struct Size {
+    var width = 0.0, height = 0.0
 }
-var item = ShoppingListItem()
-print(item.id)
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Rect {
+    var origin = Point()
+    var size = Size()
+
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+
+        origin = Point(x: originX, y: originY)
+        self.size = size
+    }
+}
+
+struct AnotherRect {
+    var origin = Point()
+    var size = Size()
+}
+
+extension AnotherRect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+//print(Rect().origin) // Missing arguments for parameters 'center', 'size' in call
+print(AnotherRect().origin)
