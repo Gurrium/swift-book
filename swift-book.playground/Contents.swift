dd74618fb8c5ab7883b9a0fa6373cbda3d3c42e8
gurrium <gurrium@hatena.ne.jp>
2021-08-02 23:54:48 +0900

    day 55 https://giarrium.hatenablog.com/entry/2021/08/02/235406?_ga=2.255282125.472858715.1627820410-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,28 +1,28 @@
-class Wrapped {
-    let num: Int
-
-    init?(_ numString: String) {
-        if let converted = Int(numString) {
-            num = converted
+struct Chessboard {
+    let boardColors: [Bool] = {
+        var temporaryBoard: [Bool] = []
+        var isBlack = false
+        for i in 1...8 {
+            for j in 1...8 {
+                temporaryBoard.append(isBlack)
+                isBlack = !isBlack
+            }
+            isBlack = !isBlack
         }
-
-        return nil
+        return temporaryBoard
+    }()
+    func squareIsBlackAt(row: Int, column: Int) -> Bool {
+        return boardColors[(row * 8) + column]
     }
 }
 
-class Unwrapped {
-    let num: Int
+let board = Chessboard()
+print(board.squareIsBlackAt(row: 0, column: 1)) // true
+print(board.squareIsBlackAt(row: 7, column: 7)) // false
 
-    init!(_ numString: String) {
-        if let converted = Int(numString) {
-            num = converted
-        } else {
-            num = 0
-        }
-    }
-}
+import Foundation
 
-// unwrapが必要
-print(Wrapped("NaN")?.num) // nil
-// unwrapが不要
-print(Unwrapped("NaN").num) // 0
+let str = "abcde12345١٢٣٤٥"
+let regex = try! NSRegularExpression(pattern: "([0-9]+)")
+let result = regex.firstMatch(in: str, range: .init(location: 0, length: str.count))!
+str[Range(result.range, in: str)!] // 12345
