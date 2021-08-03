67a77133378ed7735a7fd280c175016f72b9c7d6
gurrium <gurrium@hatena.ne.jp>
2021-08-03 21:35:47 +0900

    day 56 https://giarrium.hatenablog.com/entry/2021/08/03/213516?_ga=2.142493303.472858715.1627820410-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,28 +1,18 @@
-struct Chessboard {
-    let boardColors: [Bool] = {
-        var temporaryBoard: [Bool] = []
-        var isBlack = false
-        for i in 1...8 {
-            for j in 1...8 {
-                temporaryBoard.append(isBlack)
-                isBlack = !isBlack
-            }
-            isBlack = !isBlack
-        }
-        return temporaryBoard
-    }()
-    func squareIsBlackAt(row: Int, column: Int) -> Bool {
-        return boardColors[(row * 8) + column]
+class Sup {
+    deinit {
+        print("deinit Sup")
     }
 }
 
-let board = Chessboard()
-print(board.squareIsBlackAt(row: 0, column: 1)) // true
-print(board.squareIsBlackAt(row: 7, column: 7)) // false
+class Sub: Sup {}
 
-import Foundation
+class SubSub: Sub {
+    deinit {
+        print("deinit SubSub")
+    }
+}
 
-let str = "abcde12345١٢٣٤٥"
-let regex = try! NSRegularExpression(pattern: "([0-9]+)")
-let result = regex.firstMatch(in: str, range: .init(location: 0, length: str.count))!
-str[Range(result.range, in: str)!] // 12345
+SubSub()
+
+// deinit SubSub
+// deinit Sup
