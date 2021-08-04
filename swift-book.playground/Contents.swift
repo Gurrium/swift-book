3440c13d63e077992bf9b1bac724302bc23706af
gurrium <gurrium@hatena.ne.jp>
2021-08-04 21:02:46 +0900

    day 57 https://giarrium.hatenablog.com/entry/2021/08/04/210237?_ga=2.142426871.472858715.1627820410-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,18 +1,2 @@
-class Sup {
-    deinit {
-        print("deinit Sup")
-    }
-}
-
-class Sub: Sup {}
-
-class SubSub: Sub {
-    deinit {
-        print("deinit SubSub")
-    }
-}
-
-SubSub()
-
-// deinit SubSub
-// deinit Sup
+let hoge: String
+(hoge = "hoge") == () ? "T" : "F"
