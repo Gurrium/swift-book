e6524788905bb42f993218254fc1f9e9a444a0d1
gurrium <gurrium@hatena.ne.jp>
2021-08-05 20:34:09 +0900

    day 58 https://giarrium.hatenablog.com/entry/2021/08/05/203359?_ga=2.145599345.472858715.1627820410-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,2 +1,8 @@
-let hoge: String
-(hoge = "hoge") == () ? "T" : "F"
+struct Hoge {
+    let nonOptional: String
+    let optional: String?
+}
+
+let hoge: Hoge? = Hoge(nonOptional: "some", optional: nil)
+print(type(of: hoge?.nonOptional)) // Optional<String>
+print(type(of: hoge?.optional)) // Optional<String>
