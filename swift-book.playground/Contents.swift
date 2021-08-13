364f53e7dc39840f2e8a909ca7016b115c28465c
gurrium <gurrium@hatena.ne.jp>
2021-08-13 21:03:15 +0900

    day 61 https://giarrium.hatenablog.com/entry/2021/08/13/210259?_ga=2.51846506.291690353.1628689215-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,15 +1,39 @@
-enum Thrower {
-    struct SomeError: Error {}
+struct DummyError: Error {}
 
-    static func throwsError() throws -> Void {
-        throw SomeError()
+let closureCanThrow = {
+    defer {
+        print("first defer")
     }
+
+    defer {
+        print("second defer")
+    }
+    if Bool.random() {
+        print("throws DummyError")
+        throw DummyError()
+    }
+
+    print("not thrown")
 }
 
-enum Caller {
-    static func call() throws {
-        try Thrower.throwsError()
-    }
+for i in 1...4 {
+    print(i)
+    try? closureCanThrow()
 }
 
-try Caller.call()
+// 1
+// throws DummyError
+// second defer
+// first defer
+// 2
+// throws DummyError
+// second defer
+// first defer
+// 3
+// not thrown
+// second defer
+// first defer
+// 4
+// not thrown
+// second defer
+// first defer
