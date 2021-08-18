55b797d913958e42a75426f49d043a81e99f1683
gurrium <gurrium@hatena.ne.jp>
2021-08-18 21:46:38 +0900

    day 66 https://giarrium.hatenablog.com/entry/2021/08/18/214631?_ga=2.228624476.1727891033.1629118120-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -3,45 +3,28 @@ import _Concurrency
 
 struct Photo {}
 
-func downloadPhoto(named: String) async -> Photo {
+func uploadThumbail(_ thumbnail: Photo) async {
+    print("uploadThumbnail started")
     await Task.sleep(2 * 1_000_000_000)
-    return Photo()
+    print("uploadThumbnail ended")
+}
+func uploadTitle(_ title: String) async {
+    print("uploadTitle started")
+    await Task.sleep(2 * 1_000_000_000)
+    print("uploadTitle ended")
 }
 
 Task {
-    // unstructured concurrency
-    let handle =
-    // structured concurrency
-    Task { // parent task
-        await withTaskGroup(of: Photo.self) { taskGroup in
-            let photoNames = ["a", "b"]
-            for name in photoNames {
-                taskGroup.addTask { // child task
-                    return await downloadPhoto(named: name)
-                }
-            }
+    await withTaskGroup(of: Void.self) { taskGroup in
+        taskGroup.addTask {
+            await uploadThumbail(Photo())
+            print("first task is cancelled:", Task.isCancelled)
         }
+        taskGroup.addTask {
+            await uploadTitle("Title")
+            print("second task is cancelled:", Task.isCancelled)
+        }
+        taskGroup.cancelAll()
+        print("taskGroup is cancelled:", taskGroup.isCancelled)
     }
-    // structured concurrency
-
-    _ = await handle.result
-    // unstructured concurrency
 }
-
-//func downloadThumbail() async -> Photo { return Photo() }
-//func downloadTitle() async -> String { return "Title" }
-//struct Series {
-//    let title: String
-//    let thumbnail: Photo
-//}
-//
-//await withTaskGroup(of: Void.self) { taskGroup in
-//    taskGroup.addTask {
-//        await downloadThumbnail()
-//    }
-//    taskGroup.addTask {
-//        await downloadTitle()
-//    }
-//}
-//
-//_ = await [thumbnail, title]
