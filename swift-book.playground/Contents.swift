f5626d94cba766923b728c4484c7956c4c5725f3
gurrium <gurrium@hatena.ne.jp>
2021-08-19 21:13:23 +0900

    day 67 https://giarrium.hatenablog.com/entry/2021/08/19/211314?_ga=2.161513852.1727891033.1629118120-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,30 +1,30 @@
 import Foundation
 import _Concurrency
 
-struct Photo {}
-
-func uploadThumbail(_ thumbnail: Photo) async {
-    print("uploadThumbnail started")
-    await Task.sleep(2 * 1_000_000_000)
-    print("uploadThumbnail ended")
+protocol HogeRequirements {
+    var hoge: String { get }
 }
-func uploadTitle(_ title: String) async {
-    print("uploadTitle started")
-    await Task.sleep(2 * 1_000_000_000)
-    print("uploadTitle ended")
+
+actor Hoge: HogeRequirements {
+    let hoge: String
+
+    init(_ hoge: String) {
+        self.hoge = hoge
+    }
+
+    convenience init() {
+        self.init("hoge")
+    }
+}
+
+extension Hoge {
+    func debug() {
+        print("[Hoge] hoge:", hoge)
+    }
 }
 
 Task {
-    await withTaskGroup(of: Void.self) { taskGroup in
-        taskGroup.addTask {
-            await uploadThumbail(Photo())
-            print("first task is cancelled:", Task.isCancelled)
-        }
-        taskGroup.addTask {
-            await uploadTitle("Title")
-            print("second task is cancelled:", Task.isCancelled)
-        }
-        taskGroup.cancelAll()
-        print("taskGroup is cancelled:", taskGroup.isCancelled)
-    }
+    let hoge = Hoge()
+    print(hoge.hoge)
+    print(await hoge.debug())
 }
