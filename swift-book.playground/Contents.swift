8e006c5baa1c26b67d784a6d384219ae59a29917
gurrium <gurrium@hatena.ne.jp>
2021-08-21 21:30:40 +0900

    day 69 https://giarrium.hatenablog.com/entry/2021/08/21/69%E6%97%A5%E7%9B%AE_

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,32 +1,16 @@
-var things: [Any] = []
-
-things.append(0)
-things.append(0.0)
-things.append(42)
-things.append(3.14159)
-things.append("hello")
-things.append((3.0, 5.0))
-things.append({ (name: String) -> String in "Hello, \(name)" })
-
-for thing in things {
-    switch thing {
-    case 0 as Int:
-        print("zero as an Int")
-    case 0 as Double:
-        print("zero as a Double")
-    case let someInt as Int:
-        print("an integer value of \(someInt)")
-    case let someDouble as Double where someDouble > 0:
-        print("a positive double value of \(someDouble)")
-    case is Double:
-        print("some other double value that I don't want to print")
-    case let someString as String:
-        print("a string value of \"\(someString)\"")
-    case let (x, y) as (Double, Double):
-        print("an (x, y) point at \(x), \(y)")
-    case let stringConverter as (String) -> String:
-        print(stringConverter("Michael"))
-    default:
-        print("something else")
+struct OuterStruct {
+    struct InnerStruct {
+        let some: String
     }
+
+    actor InnerActor {
+        let immutable = "immutable"
+    }
+
+    let innerStruct: InnerStruct
+    let innerActor: InnerActor
 }
+
+let outer = OuterStruct(innerStruct: .init(some: "some"), innerActor: .init())
+outer.innerStruct.some
+outer.innerActor.immutable
