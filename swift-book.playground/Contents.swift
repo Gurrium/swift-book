04fb3e0739c32bbef19af18345478b7c1b142c80
gurrium <gurrium@hatena.ne.jp>
2021-08-23 20:12:40 +0900

    day 71 https://giarrium.hatenablog.com/entry/2021/08/23/201138?_ga=2.32279358.1000667557.1629548910-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,15 +1,41 @@
-import Foundation
+protocol Togglable {
+    mutating func toggle()
+}
 
-extension Int {
-    subscript(digitIndex: Int) -> Int {
-        var decimalBase = 1
-        for _ in 0..<digitIndex {
-            decimalBase *= 10
+enum OnOffSwitch: Togglable {
+    case off, on
+    mutating func toggle() {
+        switch self {
+        case .off:
+            self = .on
+        case .on:
+            self = .off
         }
-        return (self / decimalBase) % 10
     }
 }
 
-//12345[19]
-print(Float(Int.max) < pow(10, 18))
-print(Float(Int.max) < pow(10, 19))
+var lightSwitch = OnOffSwitch.off
+print(lightSwitch) // off
+lightSwitch.toggle()
+print(lightSwitch) // on
+
+class VideoPlayer: Togglable {
+    struct Video {}
+
+    let content: Video
+    var currentTime = 0
+    var isPlaying = false
+
+    func toggle() {
+        isPlaying = !isPlaying
+    }
+
+    init(content: Video) {
+        self.content = content
+    }
+}
+
+let player = VideoPlayer(content: .init())
+print(player.isPlaying) // false
+player.toggle()
+print(player.isPlaying) // true
