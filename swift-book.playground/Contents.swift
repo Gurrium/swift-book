0bd9ee49a44cca5a3800b0c1dc07348b5c2e4005
gurrium <gurrium@hatena.ne.jp>
2021-08-24 22:42:48 +0900

    day 72 https://giarrium.hatenablog.com/entry/2021/08/24/224241?_ga=2.85944985.1000667557.1629548910-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,41 +1,52 @@
-protocol Togglable {
-    mutating func toggle()
+protocol RandomNumberGenerator {
+    func random() -> Double
 }
 
-enum OnOffSwitch: Togglable {
-    case off, on
-    mutating func toggle() {
-        switch self {
-        case .off:
-            self = .on
-        case .on:
-            self = .off
-        }
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c)
+                        .truncatingRemainder(dividingBy:m))
+        return lastRandom / m
     }
 }
 
-var lightSwitch = OnOffSwitch.off
-print(lightSwitch) // off
-lightSwitch.toggle()
-print(lightSwitch) // on
+class RandomGenerator: RandomNumberGenerator {
+    var generator = SystemRandomNumberGenerator()
 
-class VideoPlayer: Togglable {
-    struct Video {}
-
-    let content: Video
-    var currentTime = 0
-    var isPlaying = false
-
-    func toggle() {
-        isPlaying = !isPlaying
-    }
-
-    init(content: Video) {
-        self.content = content
+    func random() -> Double {
+        return Double(UInt64.random(in: UInt64.min...UInt64.max, using: &generator)) / Double(UInt64.max)
     }
 }
 
-let player = VideoPlayer(content: .init())
-print(player.isPlaying) // false
-player.toggle()
-print(player.isPlaying) // true
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides))
+    }
+}
+
+let evaluator = { (dice: Dice) in
+    var distribution = Array(repeating: 0, count: dice.sides)
+    for _ in 0..<(dice.sides * 1000) {
+        distribution[dice.roll()] += 1
+    }
+    distribution.enumerated().forEach { index, count in
+        print("\(index + 1):", count)
+    }
+}
+
+print("LinearCongruentialGenerator")
+evaluator(Dice(sides: 6, generator: LinearCongruentialGenerator()))
+print("RandomGenerator")
+evaluator(Dice(sides: 6, generator: RandomGenerator()))
