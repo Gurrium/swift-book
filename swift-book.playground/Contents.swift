dab54221d38fe9c087ba1b6ca37015a39302063d
gurrium <gurrium@hatena.ne.jp>
2021-08-25 23:33:34 +0900

    day 73 https://giarrium.hatenablog.com/entry/2021/08/25/233324?_ga=2.267465486.1431646153.1629899054-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,52 +1,19 @@
-protocol RandomNumberGenerator {
-    func random() -> Double
+protocol SomeDelegate: AnyObject {
+    var some: String { get }
 }
 
-class LinearCongruentialGenerator: RandomNumberGenerator {
-    var lastRandom = 42.0
-    let m = 139968.0
-    let a = 3877.0
-    let c = 29573.0
-    func random() -> Double {
-        lastRandom = ((lastRandom * a + c)
-                        .truncatingRemainder(dividingBy:m))
-        return lastRandom / m
-    }
+// Non-class type 'Kome' cannot conform to class protocol 'SomeDelegate'
+// struct Kome: Some {}
+
+class Yome: SomeDelegate {
+    var some = "somep"
 }
 
-class RandomGenerator: RandomNumberGenerator {
-    var generator = SystemRandomNumberGenerator()
+class Hoge {
+    var delegate: SomeDelegate?
 
-    func random() -> Double {
-        return Double(UInt64.random(in: UInt64.min...UInt64.max, using: &generator)) / Double(UInt64.max)
-    }
+    init() {}
 }
+Hoge()
 
-class Dice {
-    let sides: Int
-    let generator: RandomNumberGenerator
-
-    init(sides: Int, generator: RandomNumberGenerator) {
-        self.sides = sides
-        self.generator = generator
-    }
-
-    func roll() -> Int {
-        return Int(generator.random() * Double(sides))
-    }
-}
-
-let evaluator = { (dice: Dice) in
-    var distribution = Array(repeating: 0, count: dice.sides)
-    for _ in 0..<(dice.sides * 1000) {
-        distribution[dice.roll()] += 1
-    }
-    distribution.enumerated().forEach { index, count in
-        print("\(index + 1):", count)
-    }
-}
-
-print("LinearCongruentialGenerator")
-evaluator(Dice(sides: 6, generator: LinearCongruentialGenerator()))
-print("RandomGenerator")
-evaluator(Dice(sides: 6, generator: RandomGenerator()))
+protocol AnyDelegate: SomeDelegate {}
