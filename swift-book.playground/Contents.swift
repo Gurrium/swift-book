c4ec8b366973307433fa16102242c2ad29775a84
gurrium <gurrium@hatena.ne.jp>
2021-08-27 22:12:26 +0900

    day 75 https://giarrium.hatenablog.com/entry/2021/08/27/220939?_ga=2.209384784.395366132.1630068325-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,7 +1,32 @@
-struct Hoge: CustomStringConvertible {
-    var description: String = "hoge"
+protocol Named {
+    var name: String { get }
 }
-let arr: [CustomStringConvertible] = [1, 1.0, "a", Hoge()]
-for e in arr {
-    print(e)
+
+class Location {
+    var latitude: Double
+    var longitude: Double
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
 }
+
+class City: Location, Named {
+    var name: String
+    init(name: String, latitude: Double, longitude: Double) {
+        self.name = name
+        super.init(latitude: latitude, longitude: longitude)
+    }
+}
+
+func beginConcert(in location: Location & Named) {
+    print("Hello, \(location.name)!")
+}
+
+let seattle = City(name: "Seattle", latitude: 47.6, longitude: -122.3)
+beginConcert(in: seattle)
+
+func test(in location: City & Named) {
+    print("location.name:", location.name)
+}
+test(in: seattle)
