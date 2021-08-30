412ad627807173e7cb7aea39c9800df1dc76037a
gurrium <gurrium@hatena.ne.jp>
2021-08-30 21:33:10 +0900

    day 77 https://giarrium.hatenablog.com/entry/2021/08/30/213253?_ga=2.28981758.395366132.1630068325-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,72 +1,27 @@
-import SwiftUI
+import Foundation
 
-struct SomeView: View {
-    var body: some View {
-        Rectangle()
-            .padding([.top, .bottom])
-            .foregroundColor(.blue)
-        Rectangle()
-            .padding(.top)
-            .padding(.bottom)
-            .foregroundColor(.red)
-    }
+protocol Hoge {
+    associatedtype FirstConstrationType
+    associatedtype SecondConstrationType
+
+    func plint()
 }
-SomeView()
-//
-//enum Custom {
-//    enum Edge {
-//        typealias Set = [Element]
-//
-//        struct Element {
-//            static let top = Self("top")
-//            static let bottom = Self("bottom")
-//
-//            let kind: String
-//            init(_ kind: String) {
-//                self.kind = kind
-//            }
-//        }
-//    }
-//}
-//
-//extension Array where Element == Custom.Edge.Element {
-//    static let top = [Element("top")]
-//    static let bottom = [Element("bottom")]
-//}
-//
-//func some(_ set: Custom.Edge.Set) {
-//    print(set)
-//}
-//
-//some(.top)
-//some([.top, .bottom])
-//some([.top, [[.bottom]]]) // error
 
-enum Custom {
-    enum Edge {
-        struct Set: ExpressibleByArrayLiteral {
-            typealias ArrayLiteralElement = Self
-
-            let rawValue: Int
-
-            static let top = Self(rawValue: 1 << 0)
-            static let bottom = Self(rawValue: 1 << 1)
-
-            init(rawValue: Int) {
-                self.rawValue = rawValue
-            }
-
-            init(arrayLiteral elements: Custom.Edge.Set...) {
-                rawValue = elements.reduce(0) { $0 | $1.rawValue }
-            }
-        }
+extension Hoge where FirstConstrationType == String {
+    func plint() {
+        print("only first")
     }
 }
 
-func some(_ set: Custom.Edge.Set) {
-    print(set)
+extension Hoge where FirstConstrationType == String, SecondConstrationType == String {
+    func plint() {
+        print("first and second")
+    }
 }
 
-some(.top)
-some([.top, .bottom])
-some([.top, [[.bottom]]])
+struct Impl: Hoge {
+    typealias FirstConstrationType = String
+    typealias SecondConstrationType = String
+}
+
+Impl().plint()
