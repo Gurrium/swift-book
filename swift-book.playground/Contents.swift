70b9c29f8afa914a16b2d9789917b4d7b23378b0
Gurrium <taira1999120@gmail.com>
2021-09-01 20:37:43 +0900

    day 79 https://giarrium.hatenablog.com/entry/2021/09/01/203720?_ga=2.137521008.1921056905.1630495816-1908612386.1612245405

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,24 +1,18 @@
-struct Stack<Element> {
-    var items: [Element] = []
-    mutating func push(_ item: Element) {
-        items.append(item)
-    }
-    mutating func pop() -> Element {
-        return items.removeLast()
-    }
+protocol Container {
+    associatedtype Item
+    
+    mutating func append(_ item: Item)
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
 }
 
-extension Stack {
-    func peek() -> Element? {
-        items.isEmpty ? nil : items[items.count - 1]
-    }
+protocol SuffixableContainer: Container {
+    associatedtype Suffix: SuffixableContainer where Suffix.Item == Item
+    func suffix(_ size: Int) -> Suffix
 }
 
-var intStack = Stack<Int>()
-intStack.push(3)
-intStack.push(5)
-intStack.push(1)
-intStack.push(8)
-if let topMost = intStack.peek() {
-    print(topMost)
+extension Array: SuffixableContainer {
+    func suffix(_ size: Int) -> [Element] {
+        Self(self[(count - size)...])
+    }
 }
