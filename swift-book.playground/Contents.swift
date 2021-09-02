61a422562758ba9cca36501d81a84bb5de3033ba
gurrium <gurrium@hatena.ne.jp>
2021-09-02 23:44:21 +0900

    idk what generics is https://giarrium.hatenablog.com/entry/2021/09/02/234323?_ga=2.120165192.1973539251.1630589953-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 protocol Container {
     associatedtype Item
     
@@ -14,5 +15,121 @@ protocol SuffixableContainer: Container {
 extension Array: SuffixableContainer {
     func suffix(_ size: Int) -> [Element] {
         Self(self[(count - size)...])
+=======
+//struct Stack<Element> {
+//    var items: [Element]
+//
+//    func peek() -> Element? {
+//        return items.count > 0 ? items[0] : nil
+//    }
+//}
+//
+//protocol Stackable {
+//    associatedtype Elementt
+//    var items: [Elementt] { get set }
+//    func peek() -> Elementt?
+//}
+//
+////extension Stack: Stackable {
+////}
+//
+//let intStack = Stack(items: [1, 2, 3])
+//print(intStack.peek())
+//
+//
+//extension Stack: Stackable where Element: Equatable, Element == Int {}
+//
+//struct EquatableStack<Element> where Element: Equatable {}
+//
+//struct NonGeneric {
+//    func nonGenericFunc() {
+//        print("nonGenericFunc")
+//    }
+//
+//    func genericFunc<T>(_ a: T) {
+//        print("genericFunc<\(T.self)>")
+//    }
+//
+//    var any: Any
+////    var int: Int
+//
+//    init<T>(_ param: T) {
+////        if T is Int {
+////            int = param
+////        } else {
+//            any = param
+////        }
+//    }
+//}
+//NonGeneric(3).genericFunc(2)
+//
+//extension Stack where Element: Equatable {}
+
+//
+//func genericFuncion<T: Equatable>(_ param: T) {
+//    print(param)
+//}
+//
+//class GenericType<T: Equatable> {
+//    var prop: T
+//
+//    init(_ prop: T) {
+//        self.prop = prop
+//    }
+//}
+//
+//protocol Prot {
+//    associatedtype T: Equatable
+//}
+
+//func genericFuncion<T, U>(_ t: T, u: U) {}
+//
+//struct GenericType<T, U> {}
+//
+//protocol Prot {
+//    associatedtype T
+//    associatedtype U
+//}
+
+func genericFuncion<T: Sequence, U>(_ t: T, u: U) where U: AnyObject, T.Element == U {}
+
+struct GenericType<T: Sequence, U> where U: AnyObject, T.Element == U {}
+
+protocol Prot {
+    associatedtype T: Sequence
+    associatedtype U: AnyObject where T.Element == U
+}
+
+//extension GenericType where T.Element == Some {
+//    func printSome() {
+//        print("T is Some")
+//    }
+//}
+//
+//extension GenericType where T.Element == Another {
+//    func printAnother() {
+//        print("T is Another")
+//    }
+//}
+//
+//class Some {}
+//class Another {}
+//GenericType<[Some], Some>().printSome() // T is Some
+// GenericType<[Another], Another>().printSome() // Referencing instance method 'printSome()' on 'GenericType' requires the types 'Another' and 'Some' be equivalent
+
+
+extension GenericType {
+    func printSome() where T.Element == Some {
+        print("T is Some")
+    }
+
+    func printAnother() where T.Element == Another {
+        print("T is Another")
+>>>>>>> Stashed changes
     }
 }
+
+class Some {}
+class Another {}
+GenericType<[Some], Some>().printSome() // T is Some
+// GenericType<[Another], Another>().printSome() // Referencing instance method 'printSome()' on 'GenericType' requires the types 'Another' and 'Some' be equivalent
