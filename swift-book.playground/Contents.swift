41f6767a6baa0f7f32ebd53d2cd2ad09849dd3eb
gurrium <gurrium@hatena.ne.jp>
2021-09-03 20:55:31 +0900

    day 80 https://giarrium.hatenablog.com/entry/2021/09/03/205523?_ga=2.12605431.1973539251.1630589953-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,117 +1,47 @@
-
-//struct Stack<Element> {
-//    var items: [Element]
-//
-//    func peek() -> Element? {
-//        return items.count > 0 ? items[0] : nil
-//    }
-//}
-//
-//protocol Stackable {
-//    associatedtype Elementt
-//    var items: [Elementt] { get set }
-//    func peek() -> Elementt?
-//}
-//
-////extension Stack: Stackable {
-////}
-//
-//let intStack = Stack(items: [1, 2, 3])
-//print(intStack.peek())
-//
-//
-//extension Stack: Stackable where Element: Equatable, Element == Int {}
-//
-//struct EquatableStack<Element> where Element: Equatable {}
-//
-//struct NonGeneric {
-//    func nonGenericFunc() {
-//        print("nonGenericFunc")
-//    }
-//
-//    func genericFunc<T>(_ a: T) {
-//        print("genericFunc<\(T.self)>")
-//    }
-//
-//    var any: Any
-////    var int: Int
-//
-//    init<T>(_ param: T) {
-////        if T is Int {
-////            int = param
-////        } else {
-//            any = param
-////        }
-//    }
-//}
-//NonGeneric(3).genericFunc(2)
-//
-//extension Stack where Element: Equatable {}
-
-//
-//func genericFuncion<T: Equatable>(_ param: T) {
-//    print(param)
-//}
-//
-//class GenericType<T: Equatable> {
-//    var prop: T
-//
-//    init(_ prop: T) {
-//        self.prop = prop
-//    }
-//}
-//
-//protocol Prot {
-//    associatedtype T: Equatable
-//}
-
-//func genericFuncion<T, U>(_ t: T, u: U) {}
-//
-//struct GenericType<T, U> {}
-//
-//protocol Prot {
-//    associatedtype T
-//    associatedtype U
-//}
-
-func genericFuncion<T: Sequence, U>(_ t: T, u: U) where U: AnyObject, T.Element == U {}
-
-struct GenericType<T: Sequence, U> where U: AnyObject, T.Element == U {}
-
-protocol Prot {
-    associatedtype T: Sequence
-    associatedtype U: AnyObject where T.Element == U
+protocol Shape {
+    func draw() -> String
 }
 
-//extension GenericType where T.Element == Some {
-//    func printSome() {
-//        print("T is Some")
-//    }
-//}
-//
-//extension GenericType where T.Element == Another {
-//    func printAnother() {
-//        print("T is Another")
-//    }
-//}
-//
-//class Some {}
-//class Another {}
-//GenericType<[Some], Some>().printSome() // T is Some
-// GenericType<[Another], Another>().printSome() // Referencing instance method 'printSome()' on 'GenericType' requires the types 'Another' and 'Some' be equivalent
-
-
-extension GenericType {
-    func printSome() where T.Element == Some {
-        print("T is Some")
-    }
-
-    func printAnother() where T.Element == Another {
-        print("T is Another")
+struct Triangle: Shape {
+    var size: Int
+    func draw() -> String {
+        var result: [String] = []
+        for length in 1...size {
+            result.append(String(repeating: "*", count: length))
+        }
+        return result.joined(separator: "\n")
     }
 }
 
-class Some {}
-class Another {}
-GenericType<[Some], Some>().printSome() // T is Some
-// GenericType<[Another], Another>().printSome() // Referencing instance method 'printSome()' on 'GenericType' requires the types 'Another' and 'Some' be equivalent
+struct FlippedShape<T: Shape>: Shape {
+    var shape: T
+    func draw() -> String {
+        let lines = shape.draw().split(separator: "\n")
+        return lines.reversed().joined(separator: "\n")
+    }
+}
+
+struct JoinedShape<T: Shape, U: Shape>: Shape {
+    var top: T
+    var bottom: U
+    func draw() -> String {
+        return top.draw() + "\n" + bottom.draw()
+    }
+}
+
+let smallTriangle = Triangle(size: 3)
+let flippedTriangle = FlippedShape(shape: smallTriangle)
+let joinedTriangles = JoinedShape(top: smallTriangle, bottom: flippedTriangle)
+print("Non-Opaque Type")
+print(joinedTriangles.draw())
+
+func flip<T: Shape>(_ shape: T) -> some Shape {
+    return FlippedShape(shape: shape)
+}
+func join<T: Shape, U: Shape>(_ top: T, _ bottom: U) -> some Shape {
+    JoinedShape(top: top, bottom: bottom)
+}
+
+let opaqueJoinedTriangles = join(smallTriangle, flip(smallTriangle))
+print("Opaque Type")
+print(opaqueJoinedTriangles.draw())
