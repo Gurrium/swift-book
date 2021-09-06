4b3e6d0fe77a8eb68752f527ad3389ddda0146c3
gurrium <gurrium@hatena.ne.jp>
2021-09-06 21:31:13 +0900

    day 83 https://giarrium.hatenablog.com/entry/2021/09/06/213106?_ga=2.166545464.784109853.1630757823-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,23 +1,90 @@
-class Ownee {
-    init() {
-        print("Ownee is initialized")
+class Department {
+    var name: String
+    var courses: [Course]
+
+    init(name: String) {
+        self.name = name
+        self.courses = []
+    }
+}
+
+protocol Course {}
+
+class WeakCourse: Course {
+    var name: String
+    unowned var department: Department
+    weak var nextCourse: UnownedCourse?
+
+    init(name: String, in department: Department) {
+        self.name = name
+        self.department = department
+        self.nextCourse = nil
     }
 
     deinit {
-        print("Ownee is deinitialized")
-    }
-}
-struct Owner {
-    weak var ownee: Ownee? {
-        didSet {
-            print("ownee is set to", ownee ?? "nil")
-        }
+        print("WeakCourse is deinitialized")
     }
 }
 
-var ownee: Ownee? = Ownee()
-var owner = Owner()
-owner.ownee = ownee
-ownee = nil
-print("owner.ownee is", owner.ownee ?? "nil")
+class UnownedCourse: Course {
+    var name: String
+    unowned var department: Department
+    weak var nextCourse: WeakCourse?
 
+    init(name: String, in department: Department) {
+        self.name = name
+        self.department = department
+        self.nextCourse = nil
+    }
+
+    deinit {
+        print("UnownedCourse is deinitialized")
+    }
+}
+
+let department = Department(name: "Horticulture")
+
+var intro: WeakCourse? = WeakCourse(name: "Survey of Plants", in: department)
+var intermediate: UnownedCourse? = UnownedCourse(name: "Growing Common Herbs", in: department)
+var advanced: WeakCourse? = WeakCourse(name: "Caring for Tropical Plants", in: department)
+
+intro?.nextCourse = intermediate
+intermediate?.nextCourse = advanced
+department.courses = [intro!, intermediate!, advanced!]
+advanced = nil
+department.courses = [intro!, intermediate!]
+intermediate!.nextCourse
+
+class Owner {
+    var weak: Weak?
+    var unow: Unow?
+}
+
+class Weak {
+    weak var own: Owner?
+
+    deinit {
+        print("Weak")
+    }
+}
+
+class Unow {
+    unowned var own: Owner?
+
+    deinit {
+        print("Unow")
+    }
+}
+
+var owner: Owner? = Owner()
+var weak: Weak? = Weak()
+var unow: Unow? = Unow()
+owner?.weak = weak
+owner?.unow = unow
+
+weak?.own = owner
+unow?.own = owner
+
+owner = nil
+print(weak?.own == nil) // true
+print(unow?.own == nil) // runtime error
