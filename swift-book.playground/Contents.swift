f50078d9f5e137170c568671544624ca5b50b8a8
gurrium <gurrium@hatena.ne.jp>
2021-09-07 21:48:05 +0900

    day 84 https://giarrium.hatenablog.com/entry/2021/09/07/214637?_ga=2.241320836.784109853.1630757823-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,90 +1,27 @@
-class Department {
-    var name: String
-    var courses: [Course]
+class HTMLElement {
+    let name: String
+    let text: String?
 
-    init(name: String) {
+    lazy var asHTML: () -> String = {
+        [unowned unownedSelf = self, unowned elem = HTMLElement(name: "h3")] in
+        if let text = unownedSelf.text {
+            return "<\(unownedSelf.name)>\(text)</\(unownedSelf.name)>"
+        } else {
+            return "<\(unownedSelf.name) />"
+        }
+    }
+
+    init(name: String, text: String? = nil) {
         self.name = name
-        self.courses = []
-    }
-}
-
-protocol Course {}
-
-class WeakCourse: Course {
-    var name: String
-    unowned var department: Department
-    weak var nextCourse: UnownedCourse?
-
-    init(name: String, in department: Department) {
-        self.name = name
-        self.department = department
-        self.nextCourse = nil
+        self.text = text
     }
 
     deinit {
-        print("WeakCourse is deinitialized")
-    }
-}
-
-class UnownedCourse: Course {
-    var name: String
-    unowned var department: Department
-    weak var nextCourse: WeakCourse?
-
-    init(name: String, in department: Department) {
-        self.name = name
-        self.department = department
-        self.nextCourse = nil
+        print("\(name) is being deinitialized")
     }
 
-    deinit {
-        print("UnownedCourse is deinitialized")
-    }
 }
 
-let department = Department(name: "Horticulture")
-
-var intro: WeakCourse? = WeakCourse(name: "Survey of Plants", in: department)
-var intermediate: UnownedCourse? = UnownedCourse(name: "Growing Common Herbs", in: department)
-var advanced: WeakCourse? = WeakCourse(name: "Caring for Tropical Plants", in: department)
-
-intro?.nextCourse = intermediate
-intermediate?.nextCourse = advanced
-department.courses = [intro!, intermediate!, advanced!]
-advanced = nil
-department.courses = [intro!, intermediate!]
-intermediate!.nextCourse
-
-class Owner {
-    var weak: Weak?
-    var unow: Unow?
-}
-
-class Weak {
-    weak var own: Owner?
-
-    deinit {
-        print("Weak")
-    }
-}
-
-class Unow {
-    unowned var own: Owner?
-
-    deinit {
-        print("Unow")
-    }
-}
-
-var owner: Owner? = Owner()
-var weak: Weak? = Weak()
-var unow: Unow? = Unow()
-owner?.weak = weak
-owner?.unow = unow
-
-weak?.own = owner
-unow?.own = owner
-
-owner = nil
-print(weak?.own == nil) // true
-print(unow?.own == nil) // runtime error
+var paragraph: HTMLElement? = HTMLElement(name: "p", text: "hello, world")
+print(paragraph!.asHTML())
+paragraph = nil
