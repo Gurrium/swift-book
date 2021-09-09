d22fd86ec0457b9eca4707c4858bac1157497da5
gurrium <gurrium@hatena.ne.jp>
2021-09-09 21:56:57 +0900

    day 86 https://giarrium.hatenablog.com/entry/2021/09/09/215642?_ga=2.131246089.784109853.1630757823-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,17 +1,38 @@
-var stepSize = 1
+//func balance(_ x: inout Int, _ y: inout Int) {
+//    let sum = x + y
+//    x = sum / 2
+//    y = sum - x
+//}
 
-func increment(_ number: inout Int) {
-    number += stepSize
+//var tup = (a: 1, b: 2)
+//balance(&tup.a, &tup.b)
+//print(tup)
+
+//_ = {
+//    func localBalance(_ x: inout Int, _ y: inout Int) {
+//        let sum = x + y
+//        x = sum / 2
+//        y = sum - x
+//    }
+//    var localTup = (a: 1, b: 3)
+//    localBalance(&localTup.a, &localTup.b)
+//    print(localTup)
+//}()
+
+var tup = (x: 0, y: 2)
+func b(_ x: inout Int, _ y: inout Int) {
+    let tmp = x
+    x = y
+    y = tmp
 }
+b(&tup.x, &tup.y) // error
 
-increment(&stepSize) // error
-
-func balance(_ x: inout Int, _ y: inout Int) {
-    let sum = x + y
-    x = sum / 2
-    y = sum - x
-}
-var playerOneScore = 42
-var playerTwoScore = 30
-balance(&playerOneScore, &playerTwoScore)
-balance(&playerOneScore, &playerOneScore) // error
+_ = {
+    var tup = (x: 0, y: 2)
+    func b(_ x: inout Int, _ y: inout Int) {
+        let tmp = x
+        x = y
+        y = tmp
+    }
+    b(&tup.x, &tup.y) // ok
+}()
