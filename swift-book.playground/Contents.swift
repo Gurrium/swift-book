12a75493a703b60b9691d6f1e2221233de55c86a
gurrium <gurrium@hatena.ne.jp>
2021-09-10 21:41:53 +0900

    day 87 https://giarrium.hatenablog.com/entry/2021/09/10/214139?_ga=2.165906104.784109853.1630757823-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,38 +1,9 @@
-//func balance(_ x: inout Int, _ y: inout Int) {
-//    let sum = x + y
-//    x = sum / 2
-//    y = sum - x
-//}
+enum Namespace {
+    struct InternalStruct {}
+    private struct PrivateStruct {}
 
-//var tup = (a: 1, b: 2)
-//balance(&tup.a, &tup.b)
-//print(tup)
-
-//_ = {
-//    func localBalance(_ x: inout Int, _ y: inout Int) {
-//        let sum = x + y
-//        x = sum / 2
-//        y = sum - x
-//    }
-//    var localTup = (a: 1, b: 3)
-//    localBalance(&localTup.a, &localTup.b)
-//    print(localTup)
-//}()
-
-var tup = (x: 0, y: 2)
-func b(_ x: inout Int, _ y: inout Int) {
-    let tmp = x
-    x = y
-    y = tmp
+    static let privateTuple = (InternalStruct(), PrivateStruct()) // Property must be declared private because its type '(Namespace.InternalStruct, Namespace.PrivateStruct)' uses a private type
+    static let internalTuple = (InternalStruct(), InternalStruct())
 }
-b(&tup.x, &tup.y) // error
 
-_ = {
-    var tup = (x: 0, y: 2)
-    func b(_ x: inout Int, _ y: inout Int) {
-        let tmp = x
-        x = y
-        y = tmp
-    }
-    b(&tup.x, &tup.y) // ok
-}()
+Namespace.internalTuple
