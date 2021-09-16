2e4ff85f9a50f6c4f39ca21a9d03d2b5db32c7be
gurrium <gurrium@hatena.ne.jp>
2021-09-16 19:19:11 +0900

    day 93 https://giarrium.hatenablog.com/entry/2021/09/16/191855?_ga=2.188121323.230475214.1631705978-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,7 +1,32 @@
-//let bits: UInt8 = 128
-//(bits << 1) >> 1
+struct Vector2D {
+    var x = 0.0, y = 0.0
+}
 
-let neg: Int8 = -1
-let pos: Int8 = 1
-print(neg >> 999)
-print(pos >> 2)
+extension Vector2D: CustomDebugStringConvertible {
+    var debugDescription: String {
+        "(\(x), \(y))"
+    }
+}
+
+extension Vector2D {
+    static func + (left: Vector2D, right: Vector2D) -> Vector2D {
+        return Vector2D(x: left.x + right.x, y: left.y + right.y)
+    }
+}
+
+extension Vector2D {
+    static prefix func - (vector: Vector2D) -> Vector2D {
+        return Vector2D(x: -vector.x, y: -vector.y)
+    }
+
+    static postfix func -- (vector: Vector2D) -> Vector2D {
+        return Vector2D(x: vector.x - 1, y: vector.y - 1)
+    }
+}
+let positive = Vector2D(x: 3.0, y: 4.0)
+let negative = -positive
+print(negative) // (-3.0, -4.0)
+let alsoPositive = -negative
+print(positive) // (3.0, 4.0)
+let decremented = positive--
+print(decremented) // (2.0, 3.0)
