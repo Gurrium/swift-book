f5aa56cd14c6d5e50ca0c060c8ed6221a4dd84f9
gurrium <gurrium@hatena.ne.jp>
2021-09-17 21:52:47 +0900

    day 94 https://giarrium.hatenablog.com/entry/2021/09/17/215239?_ga=2.130303951.1768019646.1631881942-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,32 +1,14 @@
-struct Vector2D {
-    var x = 0.0, y = 0.0
+infix operator **: MechakuchaTsuyoiPrecedence
+precedencegroup MechakuchaTsuyoiPrecedence {
+    higherThan: MultiplicationPrecedence
+    associativity: left
 }
 
-extension Vector2D: CustomDebugStringConvertible {
-    var debugDescription: String {
-        "(\(x), \(y))"
+import Foundation
+extension Int {
+    static func ** (left: Int, right: Int) -> Int {
+        Int(pow(Double(left), Double(right)))
     }
 }
 
-extension Vector2D {
-    static func + (left: Vector2D, right: Vector2D) -> Vector2D {
-        return Vector2D(x: left.x + right.x, y: left.y + right.y)
-    }
-}
-
-extension Vector2D {
-    static prefix func - (vector: Vector2D) -> Vector2D {
-        return Vector2D(x: -vector.x, y: -vector.y)
-    }
-
-    static postfix func -- (vector: Vector2D) -> Vector2D {
-        return Vector2D(x: vector.x - 1, y: vector.y - 1)
-    }
-}
-let positive = Vector2D(x: 3.0, y: 4.0)
-let negative = -positive
-print(negative) // (-3.0, -4.0)
-let alsoPositive = -negative
-print(positive) // (3.0, 4.0)
-let decremented = positive--
-print(decremented) // (2.0, 3.0)
+print(4 ** 8 * 2, "=", Int(pow(4.0, 8.0)) * 2, "≠", Int(pow(4.0, 8.0 * 2.0)))
