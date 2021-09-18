3018842e736ee18ebfe7045ead8c148051b06a94
gurrium <gurrium@hatena.ne.jp>
2021-09-18 19:47:01 +0900

    day 95 https://giarrium.hatenablog.com/entry/2021/09/18/194651?_ga=2.171860192.1768019646.1631881942-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,14 +1,72 @@
-infix operator **: MechakuchaTsuyoiPrecedence
-precedencegroup MechakuchaTsuyoiPrecedence {
-    higherThan: MultiplicationPrecedence
-    associativity: left
+protocol Drawable {
+    func draw() -> String
 }
-
-import Foundation
-extension Int {
-    static func ** (left: Int, right: Int) -> Int {
-        Int(pow(Double(left), Double(right)))
+struct Line: Drawable {
+    var elements: [Drawable]
+    func draw() -> String {
+        return elements.map { $0.draw() }.joined(separator: "")
+    }
+}
+struct Text: Drawable {
+    var content: String
+    init(_ content: String) { self.content = content }
+    func draw() -> String { return content }
+}
+struct Space: Drawable {
+    func draw() -> String { return " " }
+}
+struct Stars: Drawable {
+    var length: Int
+    func draw() -> String { return String(repeating: "*", count: length) }
+}
+struct AllCaps: Drawable {
+    var content: Drawable
+    func draw() -> String { return content.draw().uppercased() }
+}
+struct Empty: Drawable {
+    func draw() -> String {
+        ""
     }
 }
 
-print(4 ** 8 * 2, "=", Int(pow(4.0, 8.0)) * 2, "≠", Int(pow(4.0, 8.0 * 2.0)))
+@resultBuilder
+struct DrawingBuilder {
+    static func buildBlock(_ components: Drawable...) -> Drawable {
+        print("\(#function)", components)
+        return Line(elements: components)
+    }
+    static func buildEither(first: Drawable) -> Drawable {
+        print("\(#function)", first)
+        return first
+    }
+    static func buildEither(second: Drawable) -> Drawable {
+        print("\(#function)", second)
+        return second
+    }
+}
+
+func draw(@DrawingBuilder content: () -> Drawable) -> Drawable {
+    return content()
+}
+func caps(@DrawingBuilder content: () -> Drawable) -> Drawable {
+    return AllCaps(content: content())
+}
+
+func makeGreeting(for name: String? = nil) -> Drawable {
+    let greeting = draw {
+        Stars(length: 3)
+        Text("Hello")
+        Space()
+        caps {
+            if let name = name {
+                Text(name + "!")
+            } else {
+                Text("World!")
+            }
+        }
+        Stars(length: 2)
+    }
+    return greeting
+}
+let genericGreeting = makeGreeting()
+print(genericGreeting.draw())
