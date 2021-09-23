a13934548dd48868354d6b358480b6e9da6111b4
gurrium <gurrium@hatena.ne.jp>
2021-09-23 19:06:30 +0900

    day 100 https://giarrium.hatenablog.com/entry/2021/09/23/190507?_ga=2.69951571.2101372541.1632315687-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,16 +1,13 @@
-struct Some {}
-extension Some {
-    postfix static func ++ (e: Self) -> Self { e }
-    prefix static func ++ (e: Self) -> Self { e }
-    postfix static func -- (e: Self) -> Self { e }
-    prefix static func -- (e: Self) -> Self { e }
+let external: (() -> Void) -> Void = { _ in () }
+func takesTwoFunctions(first: (() -> Void) -> Void, second: (() -> Void) -> Void) {
+//    first { first {} }       // Error
+//    second { second {}  }    // Error
+//
+//    first { second {} }      // Error
+//    second { first {} }      // Error
 
-    // Operator implementation without matching operator declaration
-    postfix static func +++ (e: Self) -> Self { e }
-    // Operator implementation without matching operator declaration
-    prefix static func +++ (e: Self) -> Self { e }
-    // Operator implementation without matching operator declaration
-    postfix static func --- (e: Self) -> Self { e }
-    // Operator implementation without matching operator declaration
-    prefix static func --- (e: Self) -> Self { e }
+    first { external {} }    // OK
+    external { first {} }    // OK
 }
+
+takesTwoFunctions(first: { _ in }, second: { _ in })
