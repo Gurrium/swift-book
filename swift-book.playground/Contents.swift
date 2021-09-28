d30b0fd1d564865c266c063619367bc2b90082c0
gurrium <gurrium@hatena.ne.jp>
2021-09-28 21:18:26 +0900

    day 105 https://giarrium.hatenablog.com/entry/2021/09/28/211816?_ga=2.180682663.1818335149.1632831450-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,9 +1,16 @@
-struct Hoge {}
-print(
-    Hoge().self, // Hoge()
-    type(of: Hoge()), // Hoge
-    Hoge.self, // Hoge
-    // type(of: Hoge), // error: Expected member name or constructor call after type name
-    Hoge.self.self, // Hoge
-    type(of: Hoge.self) // Hoge.Type
-)
+let arr: [Any] = ["hoge", 1]
+
+print(type(of: arr[0]), type(of: arr.first), type(of: arr.first!)) // String Optional<Any> String
+
+class Sup {
+    func f() -> Self { self }
+    func g() -> String { "Sup" }
+}
+
+class Sub: Sup {
+    override func g() -> String { "Sub" }
+}
+
+let sub: Sup = Sub()
+print(type(of: sub.f())) // Sub
+print(sub.g()) // Sub
