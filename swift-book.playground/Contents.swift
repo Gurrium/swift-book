1be861b84bdbc58dff514f0c0bf713da2f854498
gurrium <gurrium@hatena.ne.jp>
2021-10-02 19:06:30 +0900

    day 107 https://giarrium.hatenablog.com/entry/2021/10/02/190620?_ga=2.27713018.799284367.1633087023-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,8 +1,27 @@
-func logFunctionName(string: String = #function) {
-    print(string)
-}
-func myFunction() {
-    logFunctionName() // myFunction()
+class Sup {
+    var prop = 0
+
+    subscript(_ i: Int) -> Int {
+        return i * 2
+    }
+
+    func meth() {
+        print("meth")
+    }
+
+    init() {}
 }
 
-myFunction()
+class Sub: Sup {
+    override init() {
+        super.init()
+        print(super[1])
+        print(super.prop)
+        super.meth()
+    }
+}
+
+Sub()
+// 2
+// 0
+// meth
