3548559a2b1e1ca831436e8da1a8493dc6a91db8
gurrium <gurrium@hatena.ne.jp>
2021-10-04 22:17:24 +0900

    day 110 https://giarrium.hatenablog.com/entry/2021/10/04/221715?_ga=2.36101922.367605791.1633259917-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,10 +1,8 @@
-class Some {
-    static let a = "a"
-    let b = "b"
-}
-class Sub: Some {
-    static let shared = Sub()
-    func f() -> Some { Some() }
-}
-let x: Some? = Sub().f()
-let y: Some = Sub.shared
+import Foundation
+
+let tup = (a: 1, (a: 2, b: 1), (b: 2, c: 3))
+print(tup) // (a: 1, (a: 2, b: 1), (b: 2, c: 3))
+
+print(["a": 0, "b": 1].merging(["a": 1, "b": 2]) { l, _ in l }) // ["a": 0, "b": 1]
+//print(["a": 0, "b": 1].merging(["a": 1, "b": 2]) { $0 }) // Contextual closure type '(Int, Int) throws -> Int' expects 2 arguments, but 1 was used in closure body
+print(["a": 0, "b": 1].merging(["a": 1, "b": 2]) { ($0, $1).0 }) // ["b": 1, "a": 0]
