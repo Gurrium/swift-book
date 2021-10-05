aeb00b3f00f8be35621e28c520b355830f318801
gurrium <gurrium@hatena.ne.jp>
2021-10-05 22:19:04 +0900

    day 111 https://giarrium.hatenablog.com/entry/2021/10/05/221851?_ga=2.124752776.367605791.1633259917-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,8 +1,26 @@
-import Foundation
+struct Task {
+    var description: String
+    var completed: Bool
+}
+var toDoList = [
+    Task(description: "Practice ping-pong.", completed: false),
+    Task(description: "Buy a pirate costume.", completed: true),
+    Task(description: "Visit Boston in the Fall.", completed: false),
+]
 
-let tup = (a: 1, (a: 2, b: 1), (b: 2, c: 3))
-print(tup) // (a: 1, (a: 2, b: 1), (b: 2, c: 3))
+func makeIndex() -> Int {
+    print("Made an index")
+    return Int.random(in: 0...2)
+}
 
-print(["a": 0, "b": 1].merging(["a": 1, "b": 2]) { l, _ in l }) // ["a": 0, "b": 1]
-//print(["a": 0, "b": 1].merging(["a": 1, "b": 2]) { $0 }) // Contextual closure type '(Int, Int) throws -> Int' expects 2 arguments, but 1 was used in closure body
-print(["a": 0, "b": 1].merging(["a": 1, "b": 2]) { ($0, $1).0 }) // ["b": 1, "a": 0]
+let taskKeyPath = \[Task][makeIndex()] // Made an index
+let someTask = toDoList[keyPath: taskKeyPath]
+
+extension Array where Element == Task {
+    subscript(keyPathGenerator: () -> KeyPath<Array<Element>, Element>) -> Element {
+        self[keyPath: keyPathGenerator()]
+    }
+}
+
+toDoList[makeIndex()]
+
