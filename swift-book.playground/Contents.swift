6f863b54f7eddfc0343887bda7a1c64f41931f57
gurrium <gurrium@hatena.ne.jp>
2021-10-07 20:24:17 +0900

    day 112 https://giarrium.hatenablog.com/entry/2021/10/07/202408?_ga=2.70105874.367605791.1633259917-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,4 +1,32 @@
-let opt: [Int]? = []
-opt.map { $0 }
+import Foundation
 
-// 本編 https://giarrium.hatenablog.com/entry/2021/10/06/205135?_ga=2.162053950.367605791.1633259917-1187270239.1617328348
+class SomeClass: NSObject {
+    @objc let property: String
+
+    @objc(doSomethingWithInt:)
+    func doSomething(_ x: Int) { print(x) }
+
+    init(property: String) {
+        self.property = property
+    }
+}
+let some = SomeClass(property: "prop")
+let selectorForMethod = #selector(SomeClass.doSomething(_:) as (SomeClass) -> (Int) -> Void)
+let selectorForPropertyGetter = #selector(getter: SomeClass.property)
+
+some.perform(selectorForMethod, with: 1) // 負のクソでか整数が出力される
+// わからんけどご飯ができたので終わり
+
+let unmanaged = some.perform(selectorForPropertyGetter)
+print(unmanaged?.takeRetainedValue() as! String) // prop
+unmanaged?.release()
+
+extension SomeClass {
+    @objc(doSomethingWithString:)
+    func doSomething(_ x: String) { print(x) }
+}
+let anotherSelector = #selector(SomeClass.doSomething(_:) as (SomeClass) -> (String) -> Void)
+some.perform(anotherSelector, with: "a") // a
+
+let keyPath = #keyPath(SomeClass.property)
+print(SomeClass(property: "prop").value(forKey: keyPath)!) // prop
