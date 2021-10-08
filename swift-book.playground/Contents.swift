9acdc993e64fc929b5f2da94fc35a8eb857b29ee
gurrium <gurrium@hatena.ne.jp>
2021-10-08 21:07:42 +0900

    day 113 https://giarrium.hatenablog.com/entry/2021/10/08/210727?_ga=2.66109356.367605791.1633259917-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,32 +1,17 @@
-import Foundation
-
-class SomeClass: NSObject {
-    @objc let property: String
-
-    @objc(doSomethingWithInt:)
-    func doSomething(_ x: Int) { print(x) }
-
-    init(property: String) {
-        self.property = property
-    }
+func some(_ closure: (() -> ())?) {
+    closure?()
 }
-let some = SomeClass(property: "prop")
-let selectorForMethod = #selector(SomeClass.doSomething(_:) as (SomeClass) -> (Int) -> Void)
-let selectorForPropertyGetter = #selector(getter: SomeClass.property)
 
-some.perform(selectorForMethod, with: 1) // 負のクソでか整数が出力される
-// わからんけどご飯ができたので終わり
-
-let unmanaged = some.perform(selectorForPropertyGetter)
-print(unmanaged?.takeRetainedValue() as! String) // prop
-unmanaged?.release()
-
-extension SomeClass {
-    @objc(doSomethingWithString:)
-    func doSomething(_ x: String) { print(x) }
+some {
+    print("hoge")
 }
-let anotherSelector = #selector(SomeClass.doSomething(_:) as (SomeClass) -> (String) -> Void)
-some.perform(anotherSelector, with: "a") // a
 
-let keyPath = #keyPath(SomeClass.property)
-print(SomeClass(property: "prop").value(forKey: keyPath)!) // prop
+typealias Callback = (Int) -> Int
+func someFunction(firstClosure: Callback? = nil,
+                  secondClosure: Callback? = nil) {
+    let first = firstClosure?(10)
+    let second = secondClosure?(20)
+    print(first ?? "-", second ?? "-")
+}
+
+someFunction { $0 } // - 20
