b9b1aef418fedfa0254c408ca969959bfb08e645
gurrium <gurrium@hatena.ne.jp>
2021-10-10 19:05:43 +0900

    day 115 https://giarrium.hatenablog.com/entry/2021/10/10/190529?_ga=2.117831680.809736568.1633859211-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,5 +1,8 @@
-struct Some {
-    let prop: String
+switch (1, 2) {
+// case let (x, y) where x == y, let (x, _) where x == 2: // 'y' must be bound in every pattern
+    print(x)
+case let (x, _) where x % 3 == 0, let (x, _) where x % 4 == 0:
+    print(x)
+default:
+    break
 }
-let initizlier = Some.init
-print(initizlier("hoge").prop) // hoge
