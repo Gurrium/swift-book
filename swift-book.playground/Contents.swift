cac1c1f2164b833e7e361f4857d0e73f7aad95c0
gurrium <gurrium@hatena.ne.jp>
2021-10-12 20:38:46 +0900

    day 117 https://giarrium.hatenablog.com/entry/2021/10/12/203837?_ga=2.147069118.809736568.1633859211-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,12 +1,5 @@
-switch 1 {
-case 1:
-    print("1")
-    fallthrough
-case 2:
-    print("2")
-default:
-    print("printed number")
-}
-
-// 1
-// 2
+print(#line, #file, #fileID, #filePath) // 1 swift-book.playground __lldb_expr_23/swift-book.playground swift-book.playground
+#sourceLocation(file: "/Users/gurrium/hoge", line: 100)
+print(#line, #file, #fileID, #filePath) // 100 /Users/gurrium/hoge __lldb_expr_23/hoge /Users/gurrium/hoge
+#sourceLocation()
+print(#line, #file, #fileID, #filePath) // 15 /var/folders/ys/d4cmxys124g3r621w72kjrdm0000gn/T/playground23-326d0a..swift __lldb_expr_23/playground23-326d0a..swift /var/folders/ys/d4cmxys124g3r621w72kjrdm0000gn/T/playground23-326d0a..swift
