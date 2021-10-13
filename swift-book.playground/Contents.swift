49578b4e7ef6cc9a8de5cd8d52d4d2d5f93d1827
gurrium <gurrium@hatena.ne.jp>
2021-10-13 20:24:10 +0900

    day 118 https://giarrium.hatenablog.com/entry/2021/10/13/202402?_ga=2.146142397.809736568.1633859211-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,5 +1,2 @@
-print(#line, #file, #fileID, #filePath) // 1 swift-book.playground __lldb_expr_23/swift-book.playground swift-book.playground
-#sourceLocation(file: "/Users/gurrium/hoge", line: 100)
-print(#line, #file, #fileID, #filePath) // 100 /Users/gurrium/hoge __lldb_expr_23/hoge /Users/gurrium/hoge
-#sourceLocation()
-print(#line, #file, #fileID, #filePath) // 15 /var/folders/ys/d4cmxys124g3r621w72kjrdm0000gn/T/playground23-326d0a..swift __lldb_expr_23/playground23-326d0a..swift /var/folders/ys/d4cmxys124g3r621w72kjrdm0000gn/T/playground23-326d0a..swift
+import class StoreKit.SKAdNetwork
+import let MobileCoreServices.kUTTypeUTF8PlainText
