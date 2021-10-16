5d86ad4228bf6cb374663de54cffc81d98344747
gurrium <gurrium@hatena.ne.jp>
2021-10-16 18:33:23 +0900

    day 121 https://giarrium.hatenablog.com/entry/2021/10/16/183303?_ga=2.16062582.1536376627.1634211234-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,3 +1,17 @@
-func fnc(_: Int) {}
+struct CallableStruct {
+    var value: Int
+    func callAsFunction(_ number: Int, scale: Int) {
+        print(scale * (number + value))
+    }
+    func callAsFunction() {
+        print("nothing")
+    }
+}
 
-fnc(2)
+let callable = CallableStruct(value: 1)
+callable(2, scale: 3)
+callable()
+callable.callAsFunction(2, scale: 3)
+callable.callAsFunction()
+
+var closure: ((T) -> Void)?
