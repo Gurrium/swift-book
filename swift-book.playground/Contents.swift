7d93a04f703715a9f0ee884156b5e9e20550d6aa
gurrium <gurrium@hatena.ne.jp>
2021-10-17 19:09:32 +0900

    day 122 https://giarrium.hatenablog.com/entry/2021/10/17/190924?_ga=2.101088962.291285051.1634464170-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,17 +1,16 @@
-struct CallableStruct {
-    var value: Int
-    func callAsFunction(_ number: Int, scale: Int) {
-        print(scale * (number + value))
-    }
-    func callAsFunction() {
-        print("nothing")
-    }
+struct Some {
+    func someFunc(_: () -> Void) {}
+
+    // Invalid redeclaration of 'someFunc()'
+    // func someFunc(_: () -> Void) throws {}
+
+    // 引数の型が違うのでオーバーロードできる
+    func someFunc(_: () throws -> Void) {}
 }
 
-let callable = CallableStruct(value: 1)
-callable(2, scale: 3)
-callable()
-callable.callAsFunction(2, scale: 3)
-callable.callAsFunction()
-
-var closure: ((T) -> Void)?
+func neverReturn() -> Never {
+    fatalError()
+}
+let never: Never
+never = neverReturn()
+print(never)
