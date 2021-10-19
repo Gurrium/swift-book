51229d0a5d20cf5b0705d968c33f5eca985abc0b
gurrium <gurrium@hatena.ne.jp>
2021-10-19 20:47:26 +0900

    usecase of anyobject https://giarrium.hatenablog.com/entry/2021/10/19/204714?_ga=2.192646250.1123071547.1634642586-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,12 +1,58 @@
-struct SomeSpecialID: Equatable, ExpressibleByStringLiteral {
-    let value: String
+//protocol Prot {
+//    var prop: Int { get set }
+//}
+//
+//class Cls: Prot {
+//    var prop = 0
+//}
+//struct Strct: Prot {
+//    var prop = 0
+//}
+//
+//let cls: Cls = Cls()
+//let strct: Strct = Strct()
+//cls.prop = 1
+//strct.prop = 1 // Cannot assign to property: 'strct' is a 'let' constant
+//
+//let prot: Prot = Cls()
+//prot.prop = 1 // Cannot assign to property: 'prot' is a 'let' constant
 
-    init(stringLiteral value: String) {
-        self.value = value
-    }
+protocol Prot: AnyObject {
+    var prop: Int { get set }
 }
 
-enum Other: SomeSpecialID {
-    case a, b, c
+class Cls: Prot {
+    var prop = 0
 }
-print(Other.a.rawValue)
+// Non-class type 'Strct' cannot conform to class protocol 'Prot'
+//struct Strct: Prot {
+//    var prop = 0
+//}
+
+let cls: Cls = Cls()
+cls.prop = 1
+
+let prot: Prot = Cls()
+prot.prop = 1
+
+//class Cls {
+//    private let prot: Prot
+//
+//    init(prot: Prot) {
+//        self.prot = prot
+//    }
+//
+//    func fnc() {
+//        prot.prop = 2 // Cannot assign to property: 'prot' is a 'let' constant
+//    }
+//}
+//
+//protocol Prot {
+//    var prop: Int { get set }
+//}
+//class ProtImpl: Prot {
+//    var prop = 0
+//}
+//Cls(prot: ProtImpl())
+//
+
