93d9152803247a3f5a7270013aecf21b188b3c14
gurrium <gurrium@hatena.ne.jp>
2021-10-24 18:41:12 +0900

    day 128 https://giarrium.hatenablog.com/entry/2021/10/24/184102?_ga=2.223748763.1060600219.1634998706-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,2 +1,31 @@
-// Swift Zoomin' #8
-// コードは動画公開後にpushする
+import Foundation
+
+class Cls {
+    subscript(arg param: Int) -> Int {
+        return param * 2
+    }
+}
+Cls()[arg: 1]
+
+actor SomeActor {
+    var arr = [Int]()
+
+    subscript(_ index: Int) -> Int{
+        get {
+            Thread.sleep(forTimeInterval: 0.5)
+
+            return arr[index]
+        }
+
+        set {
+            arr[index] = newValue
+        }
+    }
+}
+
+let some = SomeActor()
+Task {
+    some[0] = 0 // Actor-isolated subscript 'subscript(_:)' can not be mutated from a non-isolated context
+    some.arr.append(0) // Actor-isolated property 'arr' can not be mutated from a non-isolated context
+    print(await some[0])
+}
