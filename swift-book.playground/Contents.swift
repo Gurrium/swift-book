733e9dfd9456e9c75193335ec4837f7ad9d9e64a
gurrium <gurrium@hatena.ne.jp>
2021-11-02 20:50:13 +0900

    day 137 https://giarrium.hatenablog.com/entry/2021/11/02/204959?_ga=2.15274678.1235392005.1635852784-1187270239.1617328348

--- swift-book.playground/Contents.swift
+++ swift-book.playground/Contents.swift
@@ -1,18 +1,52 @@
-class Hoge {}
-class Fuga: Hoge {}
-
-print(Fuga.self is Hoge.Type) // true
-print(Hoge.self is Hoge.Type) // true
-
-let point = (1, 2)
-// Overload the ~= operator to match a string with an integer.
-func ~= (pattern: String, value: Int) -> Bool {
-    return pattern == "\(value)"
+protocol P {
+    associatedtype T
+    var prop: Int { get }
 }
-switch point {
-case ("0", "0"):
-    print("(0, 0) is at the origin.")
-default:
-    print("The point is at (\(point.0), \(point.1)).")
+
+protocol Q {
+    var prop: Int { get }
 }
-// Prints "The point is at (1, 2)."
+
+protocol R {
+    var prop: Int { get }
+}
+
+struct S: P, Q, R {
+    typealias T = Self
+    var prop = 0
+}
+
+extension Array {
+    // 1
+    func some() where Element: P {
+        print(1)
+    }
+
+    // 2
+//    func some() where Element: P & Q {
+//        print(2)
+//    }
+//
+//    // 3
+//    func some() where Element: P & R {
+//        print(3)
+//    }
+
+    // 4
+    func some() where Element: P, Element.T == S {
+        print(4)
+    }
+}
+
+[S()].some()
+// 1と2のとき
+// 2
+
+// 1と4のとき
+// 4
+
+// 2, 3のとき
+// Ambiguous use of 'some()'
+
+// 2, 4のとき
+// Ambiguous use of 'some()'
